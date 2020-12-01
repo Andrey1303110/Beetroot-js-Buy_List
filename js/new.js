ed59f3fa33bd7925bf33a724dbc1d4c833f3d376
Andrey1303110 <andrey1303110@gmail.com>
2020-12-01 22:49:31 +0200

    add object

--- js/new.js
+++ js/new.js
@@ -5,21 +5,26 @@ let addMessage = document.querySelector('.message'),
 let todoList = [];
 
 addButton.addEventListener('click', function () {
+
     let newToDo = {
         todo: addMessage.value,
         checked: false,
         important: false
     };
+
     todoList.push(newToDo);
-})
+    displayMessage();
+});
 
 function displayMessage() {
+    let displayMessage = '';
     todoList.forEach(function (item, i) {
-        let displayMessage = `
+        displayMessage += `
         <li>
         <input type='checkbox' id='item_${i}'>
         <label for='item_${i}'>${item.todo}</label>
         </li>`;
         todo.innerHTML = displayMessage;
+        console.log(displayMessage);
     });
 }
