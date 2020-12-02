6edd68c5199cee6db679380edb62c3255073785d
Andrey1303110 <andrey1303110@gmail.com>
2020-12-02 17:47:32 +0200

    ne poshlo

--- js/new.js
+++ js/new.js
@@ -6,11 +6,11 @@ let todoList = [];
 
 if (localStorage.getItem('todo')) {
     todoList = JSON.parse(localStorage.getItem('todo'));
-    displayMessage();
+    displayMessages();
 }
 
 addButton.addEventListener('click', function () {
-
+    if (!addMessage.value) return;
     let newToDo = {
         todo: addMessage.value,
         checked: false,
@@ -18,23 +18,48 @@ addButton.addEventListener('click', function () {
     };
 
     todoList.push(newToDo);
-    displayMessage();
+    displayMessages();
     localStorage.setItem('todo', JSON.stringify(todoList));
+    addMessage.value = '';
 });
 
-function displayMessage() {
+function displayMessages() {
     let displayMessage = '';
+    if (todoList.length === 0) todo.innerHTML = '';
     todoList.forEach(function (item, i) {
         displayMessage += `
         <li>
         <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-        <label for='item_${i}'>${item.todo}</label>
-        </li>`;
+        <label for='item_${i}'> class="${item.important ? 'important' : ''}">${item.todo}</label>
+        </li>
+        `;
         todo.innerHTML = displayMessage;
     });
 }
 
 todo.addEventListener('change', function (event) {
-    let valueLabel = todo.querySelector('[for=' + idInput + ']').innerHTML + event.target.getAttribute('id');
+    let valueLabel = todo.querySelector('[for=' + event.target.getAttribute('id') + ']').innerHTML;
 
-})
+    todoList.forEach(function (item) {
+        if (item.todo === valueLabel) {
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+
+});
+
+todo.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+    todoList.forEach(function (item, i) {
+        if (item.todo === event.target.innerHTML) {
+            if (event.ctrlKey || event.metaKey) {
+                todoList.splice(i, 1);
+            } else {
+                item.important = !item.important;
+            }
+            displayMessages();
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+});
